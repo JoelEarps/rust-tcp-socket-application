b4dc24e27f7b12ef01a4c635946bd040baa20e9f
Joel Earps <joel.earps@zenobe.com>
2024-05-07 19:56:04 +0000

    chore: session 2 commits
    Co-authored-by: Igor Braga <bragaigor@users.noreply.github.com>

--- transmitter/main.rs
+++ transmitter/main.rs
@@ -1,3 +1,23 @@
-fn main () {
-    println!("hello world");
+
+
+async fn main () {
+    let ip = "0.0.0.0";
+    test_borrow(ip);
+        
+
+
+}
+
+fn test_borrow(ip_address : &str) -> bool {
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_borrow;
+
+    #[test]
+    fn my_test() {
+        assert!(test_borrow("1.2.3.4"));
+    }    
 }
